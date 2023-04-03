7fcfc2fd883a474ac8706f211bed4c4bdc46e2dd
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-04-03 18:05:15 -0400

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) =>{
+    e.preventDefault()
+    handleTask(e.target['new-task-description'].value)
+  })
+})
+
+function handleTask(task){
+  let p = document.createElement('p')
+  console.log(p)
+  p.textContent = task
+  document.querySelector('#list').appendChild(p)
+}
